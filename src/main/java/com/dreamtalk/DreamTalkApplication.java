ecb3e7e7580ef5feed3783f8e656495e56948c7b
Qulima <mr.nekit1@gmail.com>
2021-05-17 21:09:00 +0300

    App initial commit

---
+++ src/main/java/com/dreamtalk/DreamTalkApplication.java
@@ -0,0 +1,13 @@
+package com.dreamtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamTalkApplication.class, args);
+    }
+
+}
