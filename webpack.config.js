ae965c4a9a6104975176858e563677d21931cb7b
Qulima <mr.nekit1@gmail.com>
2021-05-17 22:14:13 +0300

    Add prod config to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,63 @@
 var path = require('path')
+
+
+const prodConfig = {
+  name: 'prod.config',
+  entry: './frontend/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'target/classes/static')
+  },
+  mode: 'production',
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '.module.css', '.html', '.png']
+  },
+  module: {
+    rules: [
+      {
+        test: path.join(__dirname, '.'),
+        exclude: /(node_modles)/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }]
+      },
+      {
+        test: /\.module.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env'
+            ]
+          }
+        }]
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }]
+      },
+    ]
+  }
+}
